3ed5741579aa96ffc3a8f1d8aeeb95fb21c4afc0
Eric Santos <eric_santos2009@hotmail.com>
2022-04-28 15:43:40 -0300

    Add GetLatestRoomMessagesQuery handler

--- src/FinancialChat.Core/Features/Messages/Queries/GetLatestRoomMessagesQuery.cs
+++ src/FinancialChat.Core/Features/Messages/Queries/GetLatestRoomMessagesQuery.cs
@@ -1,6 +1,22 @@
 ﻿using FinancialChat.Core.Entities;
+using FinancialChat.Core.Interfaces;
 using MediatR;
 
 namespace FinancialChat.Core.Features.Messages.Queries;
 
 public record GetLatestRoomMessagesQuery(string Room) : IRequest<IEnumerable<ChatMessage>>;
+
+public class GetLatestRoomMessagesQueryHandler : IRequestHandler<GetLatestRoomMessagesQuery, IEnumerable<ChatMessage>>
+{
+    private readonly IChatMessageRepository _chatMessageRepository;
+
+    private const int MaxLatestMessages = 50;
+
+    public GetLatestRoomMessagesQueryHandler(IChatMessageRepository chatMessageRepository)
+    {
+        _chatMessageRepository = chatMessageRepository;
+    }
+
+    public Task<IEnumerable<ChatMessage>> Handle(GetLatestRoomMessagesQuery request, CancellationToken cancellationToken) =>
+        _chatMessageRepository.GetLatestMessagesAsync(request.Room, MaxLatestMessages);
+}
