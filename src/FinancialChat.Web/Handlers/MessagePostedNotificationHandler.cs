69074d84e66ecd413fd17a6de9a68106acce5a40
Eric Santos <eric_santos2009@hotmail.com>
2022-04-28 15:47:49 -0300

    Add MessagePostedNotification handler to send the message to SignalR

---
+++ src/FinancialChat.Web/Handlers/MessagePostedNotificationHandler.cs
@@ -0,0 +1,23 @@
+﻿using FinancialChat.Core.Notifications;
+using FinancialChat.Web.Hubs;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FinancialChat.Web.Handlers;
+
+public class MessagePostedNotificationHandler : INotificationHandler<MessagePostedNotification>
+{
+    private readonly IHubContext<ChatHub, IChatClient> _hubContext;
+
+    public MessagePostedNotificationHandler(IHubContext<ChatHub, IChatClient> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task Handle(MessagePostedNotification notification, CancellationToken cancellationToken)
+    {
+        var (message, userId, groupId) = notification;
+
+        await _hubContext.Clients.Group(groupId).ReceiveMessage(userId, message);
+    }
+}
