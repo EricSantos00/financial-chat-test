013f49337eb88f1758b4f58f12e54c1ccde9588e
Eric Santos <eric_santos2009@hotmail.com>
2022-04-29 14:37:21 -0300

    Add bot worker and listen to rabbitMQ queue

--- src/FinancialChat.Bot/Program.cs
+++ src/FinancialChat.Bot/Program.cs
@@ -1,6 +1,53 @@
-﻿namespace FinancialChat.Bot;
+﻿using FinancialChat.Bot;
+using FinancialChat.Bot.BotMessageHandler;
+using FinancialChat.Bot.MessageBroker;
+using FinancialChat.Bot.Messages;
+using FinancialChat.Bot.Services;
+using FinancialChat.Bot.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-public class Program
-{
-    
-}
+var host = Host
+    .CreateDefaultBuilder()
+    .ConfigureServices((context, services) =>
+    {
+        services.Configure<RabbitMQReceiverOptions>(context.Configuration.GetSection("RabbitMQReceiver"));
+        services.Configure<RabbitMQSenderOptions>(context.Configuration.GetSection("RabbitMQSender"));
+        services.Configure<StockSettings>(context.Configuration.GetSection("StockSettings"));
+
+        services.AddTransient<IMessageSender, RabbitMQSender>();
+        services.AddTransient<IMessageReceiver<BotCommandMessage>>(x =>
+        {
+            var options = new RabbitMQReceiverOptions();
+            context.Configuration.GetSection("RabbitMQReceiver").Bind(options);
+
+            return new RabbitMQReceiver<BotCommandMessage>(
+                new RabbitMQReceiverOptions
+                {
+                    HostName = options.HostName,
+                    UserName = options.UserName,
+                    Password = options.Password,
+                    ExchangeName = options.ExchangeName,
+                    QueueName = options.QueueName,
+                    RoutingKey = options.RoutingKey,
+                    AutomaticCreateEnabled = true
+                });
+        });
+
+        services.AddSingleton(services =>
+        {
+            var manager = new BotMessageHandlerManager(services);
+
+            manager.Register<StockCommandHandler>("/stock");
+
+            return manager;
+        });
+
+        services.AddTransient<BotService>();
+        services.AddHostedService<ChatService>();
+    })
+    .UseConsoleLifetime()
+    .Build();
+
+await host.RunAsync();
---
+++ src/FinancialChat.Bot/Worker.cs
@@ -0,0 +1,30 @@
+﻿using FinancialChat.Bot.MessageBroker;
+using FinancialChat.Bot.Messages;
+using FinancialChat.Bot.Services;
+using Microsoft.Extensions.Hosting;
+
+namespace FinancialChat.Bot;
+
+public class ChatService : BackgroundService
+{
+    private readonly IMessageReceiver<BotCommandMessage> _botCommandMessageReceiver;
+    private readonly BotService _botService;
+
+    public ChatService(IMessageReceiver<BotCommandMessage> botCommandMessageReceiver, BotService botService)
+    {
+        _botCommandMessageReceiver = botCommandMessageReceiver;
+        _botService = botService;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _botCommandMessageReceiver.Receive(async (command) =>
+        {
+            Console.WriteLine($"Received command: {command.Command}");
+
+            await _botService.ProcessCommand(command);
+        });
+
+        return Task.CompletedTask;
+    }
+}
