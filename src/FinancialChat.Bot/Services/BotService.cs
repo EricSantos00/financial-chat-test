12f4e3556a3ef13530fe5b1e9f8a9c97b7e1f874
Eric Santos <eric_santos2009@hotmail.com>
2022-04-29 14:37:05 -0300

    Add bot service orchestrator that handles the command and sends the response to the broker

---
+++ src/FinancialChat.Bot/Services/BotService.cs
@@ -0,0 +1,47 @@
+﻿using FinancialChat.Bot.BotMessageHandler;
+using FinancialChat.Bot.MessageBroker;
+using FinancialChat.Bot.Messages;
+
+namespace FinancialChat.Bot.Services;
+
+public class BotService
+{
+    private readonly BotMessageHandlerManager _botMessageHandlerManager;
+    private readonly IMessageSender _messageSender;
+
+    public BotService(BotMessageHandlerManager botMessageHandlerManager, IMessageSender messageSender)
+    {
+        _botMessageHandlerManager = botMessageHandlerManager;
+        _messageSender = messageSender;
+    }
+
+    public async Task ProcessCommand(BotCommandMessage botCommandMessage)
+    {
+        var commandResponse = await HandleCommand(botCommandMessage);
+
+        _messageSender.Publish(new BotCommandResponse(commandResponse, botCommandMessage.UserId,
+            botCommandMessage.GroupId));
+    }
+
+    private async Task<string> HandleCommand(BotCommandMessage botCommandMessage)
+    {
+        var commandSplit = botCommandMessage.Command.Split("=");
+
+        if (commandSplit.Length != 2)
+        {
+            return "Invalid command";
+        }
+
+        var command = commandSplit[0];
+        var value = commandSplit[1];
+
+        var handler = _botMessageHandlerManager.GetHandler(command);
+
+        if (handler is null)
+        {
+            return "Command not found";
+        }
+
+        return await handler.HandleMessage(value);
+    }
+}
